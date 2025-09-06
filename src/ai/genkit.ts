36921e255f7275c378b62530ef2b7cc270c606ac
Training Trade <trainingtrade82@gmail.com>
2025-09-06 06:36:29 +0000

    it is asking a billing account and i dont have online payment

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: 'AIzaSyDkCie0wdAJLorAZHPvKxNlU93pFp2Wd9Y',
     }),
   ],
   model: 'googleai/gemini-1.5-flash-latest',
